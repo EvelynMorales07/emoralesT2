903dff1a16465c39f7a590c6b9821f085bf80bb7
master <master@SMLAB304PC03_24>
2024-06-04 21:47:20 -0500

    Agregar archivos de proyecto.

---
+++ Views/vNotas.xaml.cs
@@ -0,0 +1,14 @@
+namespace emoralesT2.Views;
+
+public partial class vNotas : ContentPage
+{
+	public vNotas()
+	{
+		InitializeComponent();
+	}
+
+    private void btbCalcular_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
